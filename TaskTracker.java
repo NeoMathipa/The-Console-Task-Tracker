ef7e2f9066eb714734be527fd874b3ff568430dd
NeoMathipa <neomathipa27@gmail.com>
2026-07-14 23:23:42 +0000

    Add initial implementation of TaskTracker with basic menu options

---
+++ TaskTracker.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TaskTracker {
+    public static void main(String[] args) {
+
+    //Instance of a scanner
+    Scanner output = new Scanner(System.in);
+   
+    int choice;
+
+    while (true){
+        System.out.println("1. Add Task");
+        System.out.println("2. View Tasks");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+
+         choice = output.nextInt();
+    }
+        
+    
+}
